8215bd41d873a3c180806f1b6640e9eb3728beb2
Pablo Lima <pabloguardiao@gmail.com>
2017-08-17 18:32:08 -0300

    criação do pipe

--- src/app/foto/foto.module.ts
+++ src/app/foto/foto.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 
 import { FotoComponent } from './foto.component';
+import { FotoPipe } from './foto.pipe';
 
 @NgModule({
-  declarations: [ FotoComponent ],
+  declarations: [ FotoComponent, FotoPipe ],
   exports: [ FotoComponent ]
 })
 export class FotoModule { }
